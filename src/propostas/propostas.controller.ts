88d237e0192b85311ec02ae85afb5e05fe1ced2a
Michel Corrêa <micheloscbc@hotmail.com>
2021-08-13 23:51:25 -0300

    criando controllers e services dos módulos de propostas e usuarios

---
+++ src/propostas/propostas.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('propostas')
+export class PropostasController {}
--- src/propostas/propostas.module.ts
+++ src/propostas/propostas.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { PropostasController } from './propostas.controller';
+import { PropostasService } from './propostas.service';
 
-@Module({})
+@Module({
+  controllers: [PropostasController],
+  providers: [PropostasService]
+})
 export class PropostasModule {}
