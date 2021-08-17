4d9be1b1755e95365be387307e2fda1b941421e6
Michel Corrêa <micheloscbc@hotmail.com>
2021-08-17 20:35:07 -0300

    Adicionando metodos de atualizar e remover no controller

--- src/calculadora-de-proposta/controllers/propostas.controller.ts
+++ src/calculadora-de-proposta/controllers/propostas.controller.ts
@@ -1,6 +1,8 @@
-import { Body, Get, Post } from '@nestjs/common';
+import { Body, Delete, Get, Post } from '@nestjs/common';
+import { Patch } from '@nestjs/common';
 import { Param } from '@nestjs/common';
 import { Controller } from '@nestjs/common';
+import { ContratarPropostaDto } from '../dto/contratar-proposta-dto';
 import { CriarPropostaDto } from '../dto/criar-proposta-dto';
 import { Propostas } from '../models/propostas.model';
 import { PropostasService } from '../services/propostas.service';
@@ -10,17 +12,29 @@ export class PropostasController {
   constructor(private service: PropostasService) {}
 
   @Get()
-  GetAll() {
-    return this.service.findAll();
+  getAll() {
+    return this.service.findAllPropostas();
   }
 
   @Get('/:id')
-  GetById(@Param('id') id: string): Promise<Propostas> {
-    return this.service.findOne(id);
+  getById(@Param('id') id: string): Promise<Propostas> {
+    return this.service.getPropostas(id);
   }
 
   @Post()
   add(@Body() dto: CriarPropostaDto) {
     return this.service.add(dto);
+  }
+
+  @Patch('/:id')
+  contratar(@Param('id') idPublico: string, @Body() dto: ContratarPropostaDto) {
+    const { contratado } = dto;
+
+    return this.service.update(idPublico, contratado);
+  }
+
+  @Delete('/:id')
+  remover(@Param('id') idPublico: string) {
+    return this.service.remove(idPublico);
   }
 }
--- src/calculadora-de-proposta/services/propostas.service.ts
+++ src/calculadora-de-proposta/services/propostas.service.ts
@@ -1,6 +1,7 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { getConnection, getRepository, Repository } from 'typeorm';
+import { ContratarPropostaDto } from '../dto/contratar-proposta-dto';
 import { CriarPropostaDto } from '../dto/criar-proposta-dto';
 import { Cargas } from '../models/cargas.model';
 import { Propostas } from '../models/propostas.model';
@@ -11,14 +12,16 @@ export class PropostasService {
     @InjectRepository(Propostas) private repository: Repository<Propostas>,
   ) {}
 
-  async findAll(): Promise<Propostas[]> {
+  async findAllPropostas(): Promise<Propostas[]> {
     return await this.repository.find();
   }
 
-  async findOne(id: string): Promise<Propostas> {
-    const p = await this.repository.findOne({ id });
-
-    return p;
+  async getPropostas(idPublico: string): Promise<Propostas> {
+    const encontrado = await this.repository.findOne({ idPublico });
+    if (!encontrado) {
+      throw new NotFoundException(`Proposta de ID${idPublico} não existe.`);
+    }
+    return encontrado;
   }
 
   async add(dto: CriarPropostaDto): Promise<Propostas> {
@@ -49,5 +52,18 @@ export class PropostasService {
     proposta.cargas = [...carga];
 
     return await this.repository.manager.save(proposta);
+  }
+
+  async update(idPublico: string, contratado: boolean): Promise<Propostas> {
+    const proposta = await this.getPropostas(idPublico);
+
+    proposta.contratado = contratado;
+    await this.repository.save(proposta);
+    return proposta;
+  }
+
+  async remove(idPublico: string): Promise<void> {
+    const resultado = await this.getPropostas(idPublico);
+    await this.repository.remove(resultado);
   }
 }
