5b77cc9d22d39b82dcdb0a2f297e617628db0c1b
Michel Corrêa <micheloscbc@hotmail.com>
2021-08-20 22:19:05 -0300

    Aplicando configurações para deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,7 +15,12 @@ import { configValidationSchema } from './config.schema';
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: async (configService: ConfigService) => {
+        const isProduction = configService.get('STAGE') === 'prod';
         return {
+          ssl: isProduction,
+          extra: {
+            ssl: isProduction ? { rejectUnauthorized: false } : null,
+          },
           type: 'postgres',
           host: configService.get('DB_HOST'),
           port: configService.get('DB_PORT'),
